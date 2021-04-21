74f887dee25c0633d81d63fe5cbbbbf27645d893
lee-insu <carpediem7760@gmail.com>
2021-04-21 17:12:17 +0900

    api

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,11 @@ import TodayWeather from './components/today/today_weather/today_weather';
 import styles from './app.module.css';
 import PredictWeather from './components/predict/predict_weather/predict_weather';
 
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+
 function App() {
   return (
       <div className={styles.inner}>
