d28a4256ba25e223d5b7f6b4e2e2858eb44c04be
lee-insu <carpediem7760@gmail.com>
2021-04-22 23:46:51 +0900

    season list

--- src/components/today/today_clothes_list/today_clothes_list.jsx
+++ src/components/today/today_clothes_list/today_clothes_list.jsx
@@ -8,13 +8,30 @@ const TodayClothesList = e => {
         let season = ""
 
         switch(true) {
-            case (temp < 30):
-                season = "sum"
+            case (temp < -3):
+                season = "패딩,두꺼운 옷"
                 break;
-            case (temp < 20):
-                season = "null"
+            case (temp < 4):
+                season = "패딩,두꺼운 코트,누빔 옷, 기모, 목도리"
                 break;
-
+            case (temp < 8):
+                season = "울 코트, 히트텍, 가죽 옷, 기모"
+                break;
+            case (temp < 11):
+                season = "트렌치 코트, 야상, 점퍼, 스타킹, 기모바지"
+                break;
+            case (temp < 16):
+                season = "자켓, 가디건, 청자켓, 니트, 스타킹, 청바지"
+                break;
+            case (temp < 19):
+                season = "얇은 가디건, 니트, 맨투맨, 후드, 긴 바지"
+                break;
+            case (temp < 22):
+                season = "블라우스, 긴발티, 면바지, 슬렉스"
+                break;
+            case (temp < 27):
+                season = "반팔, 얇은 셔츠, 반바지, 면바지, 나시"
+                break; 
             default :
                 ;
 
