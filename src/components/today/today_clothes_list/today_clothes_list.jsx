94263e970c342538e0e6077257824e47647a3b41
lee-insu <carpediem7760@gmail.com>
2021-04-22 18:28:02 +0900

    clothes list

--- src/components/today/today_clothes_list/today_clothes_list.jsx
+++ src/components/today/today_clothes_list/today_clothes_list.jsx
@@ -4,10 +4,25 @@ const TodayClothesList = e => {
 
    const temp = Math.round(e.temp)-273;
 
+    
+        let season = ""
 
+        switch(true) {
+            case (temp < 30):
+                season = "sum"
+                break;
+            case (temp < 20):
+                season = "null"
+                break;
+
+            default :
+                ;
+
+            return season;
+        }
 
     return (
-        <li>hi</li>
+    <li>{season}</li>
     );
 }
 
