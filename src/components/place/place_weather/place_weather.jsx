8efb5f444372ccdacb03cf9635ce7225ce4c87ca
lee-insu <carpediem7760@gmail.com>
2021-04-27 18:05:39 +0900

    useeffect

--- src/components/place/place_weather/place_weather.jsx
+++ src/components/place/place_weather/place_weather.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PlaceWeatherCtn from '../place_weather_ctn/place_weather_ctn';
 import PlaceWeatherList from '../place_weather_list/place_weather_list';
 
@@ -9,18 +9,42 @@ const PlaceWeather = () => {
     const [weather,setWeather] = useState({});
     const [listTemp,setListTemp] = useState({});
 
-    /*
-    fetch(`https://api.openweathermap.org/data/2.5/weather?id=1835848&appid=${apiKey}`)
-    .then(res => res.json())
-    .then(result => {
-        setWeather(result);
-        setListTemp(result.main.temp);
-              });
-    console.log("ll")
-    */
-    
+   
 
-    function locationClick() {
+    
+// 수정 후 테스트 
+    useEffect(() => {
+
+        navigator.geolocation.getCurrentPosition(handleGeoSucc,handleGeoFail)
+
+        function handleGeoSucc(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+             const location = {lat,lon}
+          getWeather(location);
+      };
+
+        function handleGeoFail() {
+         console.log('location error');
+         navigator.geolocation.getCurrentPosition(handleGeoSucc,handleGeoFail)
+    }
+
+        function getWeather(location){
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`)
+            .then(res => res.json())
+            .then(result => {
+                    setWeather(result);
+                    setListTemp(result.main.temp);
+            });
+    };
+
+
+    });
+
+
+
+    /* 수정 전 
+
         navigator.geolocation.getCurrentPosition(handleGeoSucc,handleGeoFail)
     
               function handleGeoSucc(position) {
@@ -34,7 +58,7 @@ const PlaceWeather = () => {
                   console.log('location error');
                   navigator.geolocation.getCurrentPosition(handleGeoSucc,handleGeoFail)
               }
-
+              
               function getWeather(location){
                   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`)
                   .then(res => res.json())
@@ -42,15 +66,13 @@ const PlaceWeather = () => {
                           setWeather(result);
                           setListTemp(result.main.temp);
                   });
-              }
-          
-       };
+              };
 
+    */
 
    
     return(
             <>
-            <button onClick = {locationClick()}>현재 지역</button>
             <PlaceWeatherCtn weather = {weather} />
             <ul>
                 <PlaceWeatherList temp = {listTemp}/>
